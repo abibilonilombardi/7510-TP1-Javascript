132d3b5602735921de218bfc9c9d03609fcfe68d
AyeTheBest <abibiloni@fi.uba.ar>
2017-10-24 19:24:00 -0300

    V1 - pasa pruebas dadas

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,39 +1,3 @@
-// var Interpreter = function () {
-
-//     this.parseDB = function (params, paramss, paramsss) {
-
-//     }
-
-//     this.checkQuery = function (params) {
-//         return true;
-//     }
-
-// }
-
-// module.exports = Interpreter;
-
-
-db = [
-        "varon(juan).",
-        "varon(pepe).",
-        "varon(hector).",
-        "varon(roberto).",
-        "varon(alejandro).",
-        "mujer(maria).",
-        "mujer(cecilia).",
-        "padre(juan, pepe).",
-        "padre(juan, pepa).",
-        "padre(hector, maria).",
-        "padre(roberto, alejandro).",
-        "padre(roberto, cecilia).",
-        "hijo(X, Y) :- varon(X), padre(Y, X).",
-        "hija(X, Y) :- mujer(X), padre(Y, X)."
-    ];
-    
-// console.log("hija(X, Y) :- mujer(X), padre(Y, X).".match(/([a-zA-Z\s]*)\(([a-zA-Z\s,]*)\)\s*/) )
-// console.log("hija(X, Y) :- mujer(X), padre(Y, X).".match(/([a-zA-Z\s]*)\(([a-zA-Z\s,]*)\)\s*[\.|\:]?/g));
-// console.log("hija(X, Y) :- mujer(X), padre(Y, X).".split(/([a-zA-Z\s\(\)\,*][\.|\:]?/));
-
 function Parser(bdd,regex){
   
   var matchea = function(valor){
@@ -59,78 +23,97 @@ function Parser(bdd,regex){
   
 }
 
-var f = new Parser(db, /([a-zA-Z\s]*)\(([a-zA-Z\s,]*)\)\s*[\.|\:]?/);
-// console.log(f.parsearLista());
-
-var r = new Parser(db, /([a-zA-Z\s]*)\([a-zA-Z\s,]*\)\s*\:\-([a-zA-Z,()\s]*\))\.$/);
-// console.log(r.parsearLista());
-
-function Mapa(listaClaveValor){
-    var aMap = {};
-    var agregar = function(key, value) {
-      aMap[key] = aMap[key] || [];
-      aMap[key].push(value);
+function Mapa(){ //CAMBIAR NOMBRE  a multivalores
+    this.aMap = {};
+    
+    this.agregar = function(clave, valor) {
+      this.aMap[clave] = this.aMap[clave] || [];
+      this.aMap[clave].push(valor);
+      return this.aMap;
     }
-    for (i = 0; i < listaClaveValor.length ; i++){
-      var par =listaClaveValor[i];
-      agregar(par[0],par[1]);
+    
+    this.CargarMapa = function(listaClaveValor){
+      var clave = 0;
+      var valor = 1;
+      for (i = 0; i < listaClaveValor.length ; i++){
+        var tupla =listaClaveValor[i];
+        this.agregar(tupla[clave], tupla[valor]);
+      }
+      return this.aMap;
     }
-    return aMap;
+    
+    // cambiar undefined por false
+    this.contieneClave = function(clave){
+      return this.aMap[clave];
+    }
+    
+    // checkear si no null
+    this.obtenerValores = function(clave){
+      return this.aMap[clave];
+    }
 }
 
-
-var aRule = Mapa(r.parsearLista());
-var bFact = Mapa(f.parsearLista());
-console.log(aRule);
-console.log(bFact);
-
-function Reemplazar(cadena, valoresAReemplazar, valoresDeReemplazo){
-  var cadenaFinal = cadena;
-  console.log(cadena);
-  for (i = 0; i < valoresDeReemplazo.length ; i++){
-      // console.log(valoresAReemplazar[i],valoresDeReemplazo[i]);
-      cadenaFinal = cadenaFinal.replace(new RegExp(valoresAReemplazar[i], 'g'),valoresDeReemplazo[i],"g");
-      console.log(cadenaFinal)
-    }
-  return cadenaFinal;
-}
 
 var Interpreter = function () {
+  
+  var mapaRules = new Mapa();
+  var mapaFacts = new Mapa();
+  
+  this.parseDB= function(db){
+    var FactParser = new Parser(db, /([a-zA-Z\s]*)\(([a-zA-Z\s,]*)\)\s*[\.|\:]?/);
+    var RuleParser = new Parser(db, /([a-zA-Z\s]*)\([a-zA-Z\s,]*\)\s*\:\-([a-zA-Z,()\s]*\))\.$/);
+    
+    mapaFacts.CargarMapa(FactParser.parsearLista());
+    console.log("sdjsbfkajsvk");
+    console.log(mapaFacts.aMap);
+    mapaRules.CargarMapa(RuleParser.parsearLista());
+    console.log(mapaRules.aMap);
+  }
+  
+  var Reemplazar = function (cadena, valoresAReemplazar, valoresDeReemplazo){
+    var cadenaFinal = cadena;
+    // console.log(cadena);
+    for (i = 0; i < valoresDeReemplazo.length ; i++){
+        cadenaFinal = cadenaFinal.replace(new RegExp(valoresAReemplazar[i], 'g'),valoresDeReemplazo[i],"g");
+        // console.log(cadenaFinal)
+      }
+    return cadenaFinal;
+  }
 
-  this.queryArray = function(stringQuery){
+  var queryArray = function(stringQuery){
     var q = new Parser(stringQuery, /([a-zA-Z]*)\(([a-zA-Z\s,]*)\)\s*[\.|\:]?/);
     var vec = q.parsearString();
     // console.log(vec);
     return vec;
   }
   
-  this.checkearFacts= function(ClaveValor){
+  var checkearFacts= function(ClaveValor){
       console.log(ClaveValor);
-        if (bFact[ClaveValor[0]].indexOf(ClaveValor[1])!= -1){
+        if (mapaFacts.obtenerValores(ClaveValor[0]).indexOf(ClaveValor[1])!= -1){
           console.log("lo contiene");
           return true;
         }else{
           console.log("NO lo contiene");
           return false;}
     }
-    
-    
-    this.chekearRule = function( cadenaFacts){
-      var facts = cadenaFacts.match(new RegExp(/([a-zA-Z\s]*)\(([a-zA-Z\s,]*)\)/, 'g'));
-      return facts.every(fact => checkearFacts(queryArray(fact)));
-    }
-  
+
+  var chekearRule = function( cadenaFacts){
+    var facts = cadenaFacts.match(new RegExp(/([a-zA-Z\s]*)\(([a-zA-Z\s,]*)\)/, 'g'));
+    return facts.every(fact => checkearFacts(queryArray(fact)));
+  }
+
   this.checkQuery = function (query) {
     var vec = queryArray(query);
     
+    // this.parseDB(db);
     
-    if (aRule[vec[0]]){
+    if (mapaRules.contieneClave(vec[0])){ 
       console.log("es rule");
-      cadenaFacts =  Reemplazar(aRule[vec[0]][0],bFact[vec[0]][0].split(/[^a-zA-Z]/),vec[1].split(/[^a-zA-Z]/));
+      cadenaFacts =  Reemplazar(mapaRules.aMap[vec[0]][0],mapaFacts.aMap[vec[0]][0].split(/[^a-zA-Z]/),vec[1].split(/[^a-zA-Z]/));
       return chekearRule( cadenaFacts);
       
     }else{
-      if (bFact[vec[0]]){
+      if (mapaFacts.contieneClave(vec[0])){
           console.log("es fact");
           return checkearFacts(vec);
       }else{
@@ -140,7 +123,14 @@ var Interpreter = function () {
     
   }
   // return checkQuery("hijo(pepe, juan)");
-  return checkQuery('hijo(pepe, juan)');
+  // return checkQuery('hijo(pepe, juan)');
 }
 
-Interpreter()
+module.exports = Interpreter;
+
+
+
+
+
+
+
